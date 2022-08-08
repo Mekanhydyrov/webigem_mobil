7755061e8128e1e21082254b1c34015b814e2a3d
Mekan Hydyrov <mekanhydyrov@gmail.com>
2022-08-09 00:50:15 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/webigem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.webigem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Webigem',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SafeArea(child: MyHomePage()),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      body: WebView(
+      javascriptMode: JavascriptMode.unrestricted,
+      initialUrl: 'http://webigem.com/',
+    )
+   ) ;
+  }
+}
